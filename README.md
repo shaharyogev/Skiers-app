2103e8a213095152d13be91b545cd1112410c055
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2018-10-30 18:03:13 +0200

    ejs users

--- README.md
+++ README.md
@@ -1,14 +1,44 @@
+### Install Mongodb
 
-start mongodb  - 
-mongod --dbpath D:\Dropbox\Coding\Javascript\Database\database\data
+Install mongodb
+open new project folder
+install express to the folder
+add the kerberos and mongodb curent versions to the package.json file.
+```
+"mongodb": "~3.1.8",
+"kerberos": "~1.1.0"
+```
+
+npm install 
 
 conect the mongodb to env https://stackoverflow.com/questions/15053893/error-when-trying-to-connect-to-a-mongod/15055641
 
+## Start the database
+
+start nodejs - 
+```
+npm start
+```
+
+The server is on port 3000 - 
+http://localhost:3000/
+
+In a new terminal
+start mongodb: mongod --dbpath(the path to the data folder in the project)
+```
+mongod --dbpath D:\Dropbox\Coding\Javascript\Database\database\data
+
+mongo
+```
+
+or 
+
 ```
 mongod --dbpath C:\Users\Shahar Yogev LP\Dropbox\Coding\Javascript\Database\database\data
+
 mongo
 ```
 
 
-
+The database is on port 27017 - 
 http://localhost:27017/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,79 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/users',function(req,res){
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://127.0.0.1:27017/usersdb';
+
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      console.log('Unable to connect to the server', err);
+    
+    }else{ 
+      console.log('Connection Established', url);
+
+      var mydb = db.db('usersdb');
+      var collection = mydb.collection('userslist');
+      //console.log('collection ', collection);
+
+      collection.find().toArray(function(err, result){
+        if(err){
+          res.send(err);
+        }else if(result.length){
+          res.render('users',{
+            "userslist" : result
+          });
+          console.log('result ', result);
+        }else{
+          res.send('No documents found');
+        }
+        db.close();
+      });
+    }
+  });
+});
+
+
+router.get('/newuser', function(req, res){
+  res.render('newuser', {title:'Add User'});
+});
+
+
+router.post('/adduser', function(req, res){
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://127.0.0.1:27017/usersdb';
+
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      console.log('Unable to connect to the server', err);
+    
+    }else{ 
+      console.log('Connection Established', url);
+
+      var mydb = db.db('usersdb');
+      var collection = mydb.collection('userslist');
+      //console.log('collection ', collection);
+
+      var newuser ={name: req.body.name, email: req.body.email, password: req.body.password};
+      console.log('newuser ', newuser);
+
+      collection.insert([newuser], function(err, result){
+        if(err){
+          console.log('The new user did not update', err);
+        
+        }else{
+          res.redirect('users');
+        }
+        db.close();
+      });
+    }
+  });
+});
 module.exports = router;
