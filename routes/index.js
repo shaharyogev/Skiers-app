024a6efec03a1e0fd74dcc94c25b9627bd625571
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2018-11-01 00:47:06 +0200

    password bcrypt is working wall

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const someOtherPlaintextPassword = 'not_bacon';
 
 var MongoClient = mongodb.MongoClient;
 var userdbUrl = 'mongodb://127.0.0.1:27017/usersdb';
-var pasUrl ='mongodb://127.0.0.1:27017/padb';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -63,40 +63,30 @@ if(err){
 }else{ 
   var mydb = db.db('usersdb');
   var collection = mydb.collection('userslist');
-  var newuser ={name: req.body.name, email: req.body.email};
+  var userPassword = "";
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    
+  
+
+  console.log('newuser ', hash);
+  var newuser ={name: req.body.name, email: req.body.email, key: hash};
   console.log('newuser ', newuser);
 
   collection.insert([newuser], function(err, result){
     if(err){
       console.log('The new user did not update', err);
-      db.close();
+      
     }else{
-      
-    
-    db.close();
-
-    MongoClient.connect(pasUrl, function(err, pasdb){
-      if(err){
-        console.log('Unable to connect to the server', err);
-      
-      }else{
-        try{ 
-        var pascollection = pasdb.db('padb').collection('uh');
-        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-          pascollection.insert([{email: req.body.email, key: hash}]);
-          pasdb.close();
-        });
-      }catch(error){
-        console.log("pas error", error);
-      };
-      };
+      res.redirect('users');
+    };
+  });
+  db.close();
     });
-    res.redirect('users');
     }
   });
-  }
 });
-});
+
 
 router.get('/login', function(req, res){
   res.render('login', {title:'Hi User'});
@@ -108,44 +98,43 @@ router.post('/loginattempt', function(req,res){
 MongoClient.connect(userdbUrl, function(err, db){
 if(err){
   console.log('Unable to connect to the server', err);
+}
 
-}else{ 
   var mydb = db.db('usersdb');
   var collection = mydb.collection('userslist');
-  var checkUser ={password: req.body.password, email: req.body.email};
   var loginUser ={email: req.body.email};
 
-MongoClient.connect(pasUrl, function(err, pasdb){
-  if(err){
-    console.log('Unable to connect to the server', err);
-  
-  }else{
-    try{ 
-    var pascollection = pasdb.db('padb').collection('uh');
-    var testKey= pascollection.findOne({email: req.body.email});
-    console.log('The test key ',testKey.key);
+  collection.find(loginUser,{projection: {_id:0, key:1}
+    }).toArray(function(err, result){
+      if(err) throw err;
+      console.log(result[0].key);
+      bcrypt.compare(req.body.password, result[0].key, function(err, response) {
+        console.log('bcrypt response: ', response)
+        if(response){
 
-    bcrypt.compare(req.body.password, testKey, function(err, response) {
-      if(response){     
-      res.redirect('users');
-      pasdb.close();
-      
-      collection.update({email:loginUser},{$inc:{loginSuccessfully: +1}})
-      db.close();
-      }else{
-        console.log('The password is not corect');
-        pasdb.close();
-        collection.update({email:loginUser},{$inc:{loginUnsuccessfully: +1}})
-        db.close();
-      }
+          collection.updateOne(loginUser,{$inc:{loginSuccessfully: +1},function(err, res){
+            if(err)throw err;
+            console.log('The password is corect');
+            res.redirect('users');
+
+            db.close();
+          }});
+          
+
+        }else{
+        collection.updateOne(loginUser,{$inc:{loginUnsuccessfully: +1},function(err, res){
+          if(err)throw err;
+          console.log('The password is not corect');
+          db.close();
+        }});
+        }
+        
+      });
       
     });
-  }catch(error){
-    console.log("pas error", error);
-  };
-  };
+
+  });
 });
-} 
-});
-});
+
+
 module.exports = router;
