35c64789b0500c34a9170df71aae7cb815cbde77
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2018-11-20 08:37:48 +0200

    active User

--- routes/index.js
+++ routes/index.js
@@ -404,31 +404,34 @@ function topTenUsers(title, status, res){
 
 
 function mostActiveUser(title, status, res){
-  collection.aggregate([{$unwind: '$activeUsers' }]).toArray(function(err, result){
+  collection.aggregate([{$unwind: '$activeUsers' },{$project:{_id:0, title:1, activeUsers:1}},{$group:{_id: '$activeUsers.email', rentedMovies: { $push:{ titel:'$title', inventory:'$activeUsers.inventory'} }}}]).toArray(function(err, result){
     if(err) console.log(err)
-
+    console.log(result);
     let temp =[];
     let obj =null;
     let userTemp = [];
 
-
+/*
     for(let i = 0; result.length>i; i++){
       obj = result[i];
       
+      
       if(!temp[obj.activeUsers.email])
-        temp[obj.activeUsers.email] = {title: obj.title, inventory:obj.activeUsers.inventory }
+        temp[obj.activeUsers.email] = [],
+        temp[obj.activeUsers.email].push({email: obj.activeUsers.email, title: obj.title, inventory: obj.activeUsers.inventory });
       
       else
-        temp[obj.activeUsers.email] += {title: obj.title, inventory:obj.activeUsers.inventory }
+        temp[obj.activeUsers.email].push({email: obj.activeUsers.email, title: obj.title, inventory:obj.activeUsers.inventory })
         //console.log('temp[obj.activeUsers.email: ',temp[obj.activeUsers.email]);
 
     }
-
+      //console.log('temp :', temp)
     for(let index in temp){
-      userTemp.push(temp[index])
+      //userTemp.push({email:temp[index])
       //console.log('userTemp ', index, ' : ', userTemp);
     }
     //console.log(result);
+    */
   })
   
   //res.render('movies', { movieslist: [], userslist: usersList, title: status, status:  title });
