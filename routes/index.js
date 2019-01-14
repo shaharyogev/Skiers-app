4f392a70426291423828533926ef23e99f42a491
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-01-14 21:54:12 +0200

    login screen

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const someOtherPlaintextPassword = 'not_bacon';
-const MongoClient = mongodb.MongoClient;
 const usersdbUrl = 'mongodb://127.0.0.1:27017/usersdb';
+const client = MongoClient(usersdbUrl, {
+  useNewUrlParser: true
+});
 const session = require('express-session');
 const expressValidator = require('express-validator');
 const formidableMiddleware = require('express-formidable');
 const async = require("async");
+let inviteListForLogIn
+
 
 
 
@@ -39,7 +43,7 @@ const async = require("async");
 
 /*Start the Database connection: */
 
-MongoClient.connect(usersdbUrl, function (err, db) {
+client.connect(function (err, db) {
   if (err)
     console.log(err.stack);
 
@@ -88,6 +92,7 @@ MongoClient.connect(usersdbUrl, function (err, db) {
     res.render('login');
   })
 
+  
   //While the user in session the system will stay active.
 
   router.get('/:id', function (req, res, next) {
@@ -99,45 +104,66 @@ MongoClient.connect(usersdbUrl, function (err, db) {
 
   //Handle user login request
 
-  router.post('/user', expressValidator(), function (req, res) {
+  router.post('/user', formidableMiddleware(), function (req, res) {
 
-    let email = req.body.email;
-    let password = req.body.password;
+    let email = req.fields.email;
+    console.log(email);
 
-    req.checkBody('email', 'Name is required').notEmpty();
-    req.checkBody('email', 'Name is required').isEmail();
-    req.checkBody('password', 'Name is required').notEmpty();
+    let errors;
 
-    const errors = req.validationErrors();
+    if (email != undefined) {
+      let re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      let res = re.test(String(email).toLowerCase());
+      console.log('email is: ' + res);
+      if (!res) {
+        errors = 'Hi ' + email + ' You cant use this email, try a real one';
+      }
+    }
+
+    let password = req.fields.password;
+    if (password != undefined) {
+      let re =
+        /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\S+$).{5,}$/;
+      let res = re.test(String(password));
+      console.log('password is: ' + res);
+      if (!res) {
+        errors += ' the assword is Minmum 5 character long + upper cases + lower cases + digits ';
+      }
+    }
+    console.log(password);
+
+    console.log(errors);
     if (errors) {
       req.session.errors = errors;
-      res.redirect('/login');
+      res.send({
+        err: errors
+      });
       console.log('loginAttempt fail');
     } else {
-      loginAttempt(req.body.email, req.body.password, req, res);
+      loginAttempt(req.fields.email, req.fields.password, req, res);
+
+
     }
   });
 
   //Handle new user singUp
 
-  router.post('/addUser', expressValidator(), function (req, res) {
-    let name = req.body.name;
-    let email = req.body.email;
-    let password = req.body.password;
+  router.post('/addUser', formidableMiddleware(), function (req, res, next) {
+    let invite = req.fields.invite;
+    let name = req.fields.name;
+    let email = req.fields.email;
+    let password = req.fields.password;
 
-    req.checkBody('name', 'Name is required').notEmpty();
-    req.checkBody('email', 'Name is required').notEmpty();
-    req.checkBody('email', 'Name is required').isEmail();
-    req.checkBody('password', 'Name is required').notEmpty();
-
-    const errors = req.validationErrors();
+    let errors;
     if (errors) {
       req.session.errors = errors;
       res.redirect('/login');
     } else {
-      creatNewUser(req.body.name, req.body.email, req.body.password, req, res);
+      creatNewUser(req.fields.invite, req.fields.name, req.fields.email, req.fields.password, req, res);
     }
   });
+
 
 
   /* Qureys routes for the database */
@@ -149,7 +175,7 @@ MongoClient.connect(usersdbUrl, function (err, db) {
   router.post('/submitrent', formidableMiddleware(), function (req, res) {
     updateRentedInventory(req.fields.title, req.fields.inventory, req.fields.email, res);
   });
-  
+
   router.post('/submitNewCustomer', formidableMiddleware(), function (req, res) {
     submitNewCustomer(req.fields.name, req.fields.email, req.fields.phone, res)
   });
@@ -183,48 +209,107 @@ MongoClient.connect(usersdbUrl, function (err, db) {
   });
 
 
+  function testInviteListForLogin(invite) {
+    return new Promise(resolve => {
+      usersCollection.findOne({
+        inviteHash: 'tempInvite'
+      }, {
+        projection: {
+          _id: 0,
+          key: 1,
+        }
+      }, function (err, r) {
+        if (err) console.log(err);
+
+        if (r !== null) {
+          bcrypt.compare(invite, r.key, function (err, bcRes) {
+            if (err) console.log(err);
+            if (bcRes) {
+              console.log("66");
+              resolve(true);
+            } else {
+              console.log("99");
+              resulve(false);
+            }
+          })
+        }
+      })
+    })
+  }
+
+  function inviteListForLogin(invite) {
+    return new Promise(resolve => {
+      bcrypt.hash(invite, saltRounds, function (err, hash) {
+
+        usersCollection.findOneAndUpdate({
+          inviteHash: 'tempInvite'
+        }, {
+          $set: {
+            key: hash
+          }
+        }, function (err, r) {
+          if (err) console.log(err);
+          if (r !== null) {
+            resolve(true);
+          } else {
+            resulve(false);
+          }
+        })
+      })
+    })
+  }
+
+
   /* User login and creation: */
 
   //Creat New User - callback inventoryStatusInLogin
 
-  function creatNewUser(name, email, password, req, res) {
-    if (name)
-      name = name.toLowerCase();
+  async function creatNewUser(invite, name, email, password, req, res) {
+    try {
+      const testInvite = await testInviteListForLogin(invite);
+      if (testInvite) {
+        if (name)
+          name = name.toLowerCase();
 
-    if (email)
-      email = email.toLowerCase();
+        if (email)
+          email = email.toLowerCase();
 
-    if (password)
-      bcrypt.hash(password, saltRounds, function (err, hash) {
+        if (password)
+          bcrypt.hash(password, saltRounds, function (err, hash) {
 
-        usersCollection.findOne({
-          email: email
-        }, function (err, r) {
-          if (err) console.log(err);
-          if (r == null) {
-            usersCollection.insertOne({
-              userName: name,
-              email: email,
-              key: hash
+            usersCollection.findOne({
+              email: email
             }, function (err, r) {
               if (err) console.log(err);
-              if (r !== null) {
-                if (r.result.n == 1) {
-                  ;
-                  // Callback if success 
-                  startUserSession(email, 'Most Avialebel Inventory:', '', '', req, res);
-                  //req.session.succes = true
-                  //inventoryStatusInLogin('hello new user', '', name, res);
-                }
+              if (r == null) {
+                usersCollection.insertOne({
+                  userName: name,
+                  email: email,
+                  key: hash
+                }, function (err, r) {
+                  if (err) console.log(err);
+                  if (r !== null) {
+                    if (r.result.n == 1) {;
+                      // Callback if success 
+                      startUserSession(email, 'Most Avialebel Inventory:', '', '', req, res);
+                      //req.session.succes = true
+                      //inventoryStatusInLogin('hello new user', '', name, res);
+                    }
+                  } else {
+                    res.render('login')
+                  }
+                })
               } else {
                 res.render('login')
               }
             })
-          } else {
-            res.render('login')
-          }
-        })
-      })
+          })
+      } else {
+        res.render('login')
+      }
+    } catch (err) {
+      console.log(err.stack);
+    }
   };
 
 
@@ -299,48 +384,55 @@ MongoClient.connect(usersdbUrl, function (err, db) {
       if (err) console.log(err)
       if (r !== null) {
         req.session.succes = true,
-        req.session.cookie = {
-          name: 'moviesInventory',
-          userName: r.userName,
-          originalMaxAge: 1000 * 60 * 60 * 24 * 365
-        };
-        inventoryStatusInLogin(title, status, r.userName, res);
+          req.session.cookie = {
+            name: 'moviesInventory',
+            userName: r.userName,
+            originalMaxAge: 1000 * 60 * 60 * 24 * 365
+          };
+        inventoryStatusInLogin(title, status, r.userName, req, res);
       }
     })
   }
 
 
-/* New Database functions: */
+  /* New Database functions: */
 
-async function submitNewCustomer( name, email, phone, res){
-  try{
-  let query = {};
+  async function submitNewCustomer(name, email, phone, res) {
+    try {
+      let query = {};
 
-  if (name)
-    query.name = name;
+      if (name)
+        query.name = name;
 
-  if (phone)
-    query.phone = phone;
+      if (phone)
+        query.phone = phone;
 
-  if (email)
-    email = email.toLowerCase(),
-    query.email = email;
-  
-  let r = await collection.findOneAndUpdate({'email':email},{$set:query},{
-    upsert:true,
-    projection:{'_id':0, 'email':1},
-    returnNewDocument:true
-  });
+      if (email)
+        email = email.toLowerCase(),
+        query.email = email;
 
-  await res.send({
-    moviesList: r.value,
-    title: name,
-    status: phone
-  });
-  } catch(err){
-    console.log(err.stack);
-  }
-};
+      let r = await collection.findOneAndUpdate({
+        'email': email
+      }, {
+        $set: query
+      }, {
+        upsert: true,
+        projection: {
+          '_id': 0,
+          'email': 1
+        },
+        returnNewDocument: true
+      });
+
+      await res.send({
+        moviesList: r.value,
+        title: name,
+        status: phone
+      });
+    } catch (err) {
+      console.log(err.stack);
+    }
+  };
 
 
 
@@ -464,9 +556,9 @@ async function submitNewCustomer( name, email, phone, res){
 
         else
           currentUserInventory(title, email, function (err, value) {
-          title = title + ' inventory was updated to' + (r.value.inventory - inventory),
-          status = 'The user: ' + email + ' have ' + value + ' items include the ' + inventory + ' new items',
-          inventoryStatus(title, status, '', res);
+            title = title + ' inventory was updated to' + (r.value.inventory - inventory),
+              status = 'The user: ' + email + ' have ' + value + ' items include the ' + inventory + ' new items',
+              inventoryStatus(title, status, '', res);
           })
       })
   };
@@ -526,7 +618,7 @@ async function submitNewCustomer( name, email, phone, res){
   };
 
 
-  
+
 
 
   /* Databas queries: */
@@ -709,7 +801,7 @@ async function submitNewCustomer( name, email, phone, res){
     })
   };
 
-  function inventoryStatusInLogin(title, status, userName, res) {
+  function inventoryStatusInLogin(title, status, userName,req, res) {
     collection.aggregate([{
         $project: {
           _id: 0,
@@ -738,6 +830,10 @@ async function submitNewCustomer( name, email, phone, res){
         let temp = 'Item: ' + result[index].title + ' Avialebel inventory: ' + result[index].inventory;
         moviesList.push(temp);
       }
+      console.log('ok1');
+
+
+
       res.render('movies', {
         moviesList: moviesList,
         title: title,
